6f000f83424c6c66db8694d9a2ca617993217c04
David Van Horn <dvanhorn@cs.umd.edu>
2024-10-16 18:01:26 -0400

    Add dep.

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define version "1.0")
 (define collection 'multi)
-(define deps (list "base" "rackunit" "redex-lib"))
+(define deps (list "base" "rackunit" "redex-lib" "redex-gui-lib"))
 (define scribblings (list "a86/scribblings"))
