8cf6809c1f77569676a3399b13caf8d9b96718e5
David Van Horn <dvanhorn@cs.umd.edu>
2024-12-11 20:30:02 -0500

    Add warning for nasm version; avoid some tests if too old.

--- a86/check-nasm.rkt
+++ a86/check-nasm.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(provide check-nasm-available)
+(provide check-nasm-available nasm-version nasm-version-2.15+?)
 (require racket/gui/dynamic)
 
 (define nasm-msg
@@ -30,10 +30,33 @@ HERE
 (define (drracket?)
   (gui-available?))
 
+;; -> [Maybe String]
+(define (nasm-version-string)
+  (parameterize ([current-output-port (open-output-string)]
+                 [current-error-port (open-output-string)])
+    (and (system "nasm -v")
+         (get-output-string (current-output-port)))))
+
+(define (nasm-version-2.15+?)
+  (match (nasm-version)
+    [(list maj min) (and (>= maj 2) (>= min 15))]
+    [_ #f]))
+
+;; -> [Maybe (list Natural Natural)]
+(define (nasm-version)
+  (match (nasm-version-string)
+    [#f #f]
+    [(regexp #rx"([0-9]+)\\.([0-9]+)"
+             (list _ (app string->number maj) (app string->number min)))
+     (list maj min)]))
+
+;; -> Void
+;; Errors if nasm is not available, warns if available but below 2.15
 (define (check-nasm-available)
-  (unless (parameterize ([current-output-port (open-output-string)]
-                         [current-error-port (open-output-string)])
-            (system "nasm -v"))
+  (define v (nasm-version))
+  (unless v
     (error (format nasm-msg
                    (getenv "PATH")
-                   (if (and (drracket?) (macos?) (launched-with-finder?))  macosx-msg "")))))
+                   (if (and (drracket?) (macos?) (launched-with-finder?))  macosx-msg ""))))
+  (unless (nasm-version-2.15+?)
+    (eprintf "nasm 2.15 or later is recommended; some faatures may not work as expected.")))
