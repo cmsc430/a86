842e053cbda9e87382ea9d00714af24e257da99b
David Van Horn <dvanhorn@cs.umd.edu>
2026-04-08 16:24:39 -0400

    Tweaks to printing; ignore section tests.
    + Use double quotes on label names to fix #40.
    + Provide a private parameter to control the os type when printing.
    + Ignore section tests until #43 is resolved.

--- a86/info.rkt
+++ a86/info.rkt
@@ -4,5 +4,6 @@
 (define deps (list "base" "rackunit" "redex-lib" "redex-gui-lib"))
 (define scribblings '(("scribblings/a86.scrbl")))
 (define test-omit-paths '("scribblings/"
-                          "test/expressions.rkt"))
+                          "test/expressions.rkt"
+                          "test/sections.rkt"))
 (define pre-install-collection "check-x86.rkt")
